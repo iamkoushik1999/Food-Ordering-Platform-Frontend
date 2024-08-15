50da70c42ac02fac2f09aa2f4e82e7be83845b55
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-08-15 23:29:03 +0530

    Authenticated user links

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,26 +1,51 @@
-import React from "react";
-import { Menu } from "lucide-react";
-import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet";
-import { Separator } from "./ui/separator";
-import { Button } from "./ui/button";
+import React from 'react';
+import { CircleUserRound, Menu } from 'lucide-react';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from './ui/sheet';
+import { Separator } from './ui/separator';
+import { Button } from './ui/button';
+import { useAuth0 } from '@auth0/auth0-react';
+import MobileNavLinks from './MobileNavLinks';
 
 const MobileNav = () => {
-  return(
-   <Sheet>
+  const { loginWithRedirect, isAuthenticated, user } = useAuth0();
+
+  return (
+    <Sheet>
       <SheetTrigger>
-         <Menu className="text-orange-500"/>
+        <Menu className='text-orange-500' />
       </SheetTrigger>
-      <SheetContent className="space-y-3">
-         <SheetTitle>
+      <SheetContent className='space-y-3'>
+        <SheetTitle>
+          {isAuthenticated ? (
+            <span className='flex items-center font-bold gap-2'>
+              <CircleUserRound className='text-orange-500' />
+              {user?.email}
+            </span>
+          ) : (
             <span>Welcome to Eats.com</span>
-         </SheetTitle>
-         <Separator/>
-         <SheetDescription className="flex" >
-            <Button className="flex-1 fontb bg-orange-500">Login</Button>
-         </SheetDescription>
+          )}
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className='flex flex-col gap-4'>
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button
+              onClick={async () => await loginWithRedirect()}
+              className='flex-1 fontb bg-orange-500'>
+              Login
+            </Button>
+          )}
+        </SheetDescription>
       </SheetContent>
-   </Sheet>
-  )
-}
+    </Sheet>
+  );
+};
 
 export default MobileNav;
