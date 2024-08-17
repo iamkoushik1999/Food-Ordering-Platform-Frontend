76a0cb69cc8c45861512794ce77539520fdf34e4
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-08-17 22:44:45 +0530

    Create user request

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { AppState, Auth0Provider, User } from '@auth0/auth0-react';
+import { useCreateMyUser } from '@/api/MyUserApi';
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser } = useCreateMyUser();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -15,7 +17,13 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   }
 
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log('USER', user);
+    // console.log('USER', user);
+    if (user?.sub && user?.email) {
+      createUser({
+        auth0Id: user.sub,
+        email: user.email,
+      });
+    }
   };
 
   return (
